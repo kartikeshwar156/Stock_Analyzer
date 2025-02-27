e6f1305b313e79c7b721c69b95a5021f3a70e44a
kartikeshwar <kartikeshwarhingole@gmail.com>
2025-02-28 00:36:17 +0530

    added base line

--- BSE_Stock_Analyzer/vite.config.js
+++ BSE_Stock_Analyzer/vite.config.js
@@ -4,7 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Stock_Analyzer/", // Use your repository name here,
   build: {
     outDir: "build" // Ensure Vite builds into dist/
   }
 })
+
+// How Does This Help?
+
+// Before:
+//     Vite generates asset links like /assets/index.js, but GitHub Pages looks for /Stock_Analyzer/assets/index.js, causing 404 errors.
+// After setting base correctly:
+//     Vite generates links as /Stock_Analyzer/assets/index.js, so GitHub Pages finds them correctly.
