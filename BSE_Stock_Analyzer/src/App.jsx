cce3d2639abf8b73d8fd62b1df68fe9dc25dd61b
kartikeshwar <kartikeshwarhingole@gmail.com>
2025-02-28 19:09:20 +0530

    using ngrok to give a new link

--- BSE_Stock_Analyzer/src/App.jsx
+++ BSE_Stock_Analyzer/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     
 
     try {
-      const res = await axios.post("http://localhost:8080/query", {
+      const res = await axios.post("https://7dc2-2401-4900-1f27-ea90-3547-492f-8d01-434d.ngrok-free.app/query", {
         query,
       });
       console.log("API Response:", res.data);
